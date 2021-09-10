f8bdbb0037088a274ce40aef30cfdbb3cc32876a
Zeusz0 <2000zeno@gmail.com>
2021-09-10 15:17:15 +0200

    User activity monitoring and refactor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { TwitterApi } from 'twitter-api-v2';
-import {config} from "dotenv"
-config()
+import { config } from 'dotenv';
+config();
 
 const apikey = process.env.apikey;
 const apiSecKey = process.env.apiSecKey;
@@ -8,36 +8,68 @@ const accToken = process.env.accToken;
 const accTokenSec = process.env.accTokenSec;
 
 const client = new TwitterApi({
-    appKey: apikey,
-    appSecret: apiSecKey,
-    accessToken: accToken,
-    accessSecret: accTokenSec,
+  appKey: apikey,
+  appSecret: apiSecKey,
+  accessToken: accToken,
+  accessSecret: accTokenSec,
 });
 
 // Gets the useres, who liked the tweet specified by the param
 // Returns with an array of the likers' IDs
-async function getLikersById(tweetId:string) {
-    let likersIds = (await client.v2.tweetLikedBy(tweetId)).data.map(like =>like.id);
-    //let likersIds = (await client.v2.tweetLikedBy(tweetId));
-    return  likersIds;
+async function getLikersById(tweetId: string) {
+  const tweetLikers = await client.v2.tweetLikedBy(tweetId);
+  return tweetLikers.data?.map((like) => like.id) ?? [];
 }
 
-//console.log(await getLikersById('1433837647189917700'));
+async function getLikersByIds(tweetIds: string[]) {
+  return Promise.all(tweetIds.map(getLikersById));
+}
 
 // Gets the user's ID and Wallet adress by the tweets ID
-//Returns with the Address and the Users's Twitter ID 
-async function getUserAndAddressById(tweetId:string):Promise<[string,string]> {
-    const  [{ full_text, user: { id_str: userId } }] :any[] = await client.v1.tweets(tweetId);
-    return  [/0x[a-zA-Z0-9]{40}/.exec(full_text)![0],userId]
-} 
-
-//console.log(await name("1434123996958707717"));
+//Returns with the Address and the Users's Twitter ID
+async function getUserAndAddressById(
+  tweetId: string,
+): Promise<[string, string]> {
+  const [
+    {
+      full_text,
+      user: { id_str: userId },
+    },
+  ]: any[] = await client.v1.tweets(tweetId);
+  return [/0x[a-zA-Z0-9]{40}/.exec(full_text)![0], userId];
+}
 
 // Gets the useres, who retweeted the tweet specified by the param
 // Returns with an array of the reteeres' IDs
-async function getRetweetersById(tweetId:string) {
-    let retweetersId = (await client.v2.tweetRetweetedBy(tweetId)).data.map(retweet =>retweet.id);
-    return retweetersId;
+async function getRetweetersById(tweetId: string) {
+  const tweetLikers = await client.v2.tweetRetweetedBy(tweetId);
+  return tweetLikers.data?.map((like) => like.id) ?? [];
 }
 
-//console.log(await getRetweetersById("1436035119610015744"));
+async function getRetweetersByIds(tweetIds: string[]) {
+  return Promise.all(tweetIds.map(getRetweetersById));
+}
+
+async function getUserActivity(userIds: string[], tweetIds: string[]) {
+  const tweetData = await Promise.all(
+    Array.from(new Set(tweetIds)).map((tweetId) =>
+      Promise.all([getRetweetersById(tweetId), getLikersById(tweetId)]).then(
+        ([retweets, likes]) => ({ retweets, likes }),
+      ),
+    ),
+  );
+
+  const userIdPairs = Object.fromEntries(userIds.map((id) => [id, 0]));
+
+  tweetData.forEach(({ retweets, likes }) => {
+    retweets.concat(likes).forEach((id) => {
+      if (id in userIdPairs) {
+        userIdPairs[id]++;
+      }
+    });
+  });
+
+  return userIdPairs;
+}
+
+console.log(await getRetweetersByIds(['1435209340819021824', '0']));
