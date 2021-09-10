1cdbdedfe1b9785720fe1444ca64d69f74e70018
Zeusz0 <2000zeno@gmail.com>
2021-09-10 08:34:40 +0200

    config change, so unused things won't throw errors

--- src/main.ts
+++ src/main.ts
@@ -14,12 +14,30 @@ const client = new TwitterApi({
     accessSecret: accTokenSec,
 });
 
-//let likersIds = (await client.v2.tweetLikedBy('1433837647189917700')).data.map(like =>like.id);
-//client.v2.tweetLikedBy('1433837647189917700').then(console.log);
-//console.log(likersIds);
-async function name(tweetId:string):Promise<[string,string]> {
-    const  [{ full_text, user: { id_str: userId } }] :any[] = await client.v1.tweets(tweetId);
-    return  [/0x[a-zA-Z0-9]{40}/.exec(full_text)![0],userId]
+// Gets the useres, who liked the tweet specified by the param
+// Returns with an array of the likers' IDs
+async function getLikersById(tweetId:string) {
+    let likersIds = (await client.v2.tweetLikedBy(tweetId)).data.map(like =>like.id);
+    //let likersIds = (await client.v2.tweetLikedBy(tweetId));
+    return  likersIds;
 }
 
-console.log(await name("1434123996958707717"));
+//console.log(await getLikersById('1433837647189917700'));
+
+// Gets the user's ID and Wallet adress by the tweets ID
+//Returns with the Address and the Users's Twitter ID 
+async function getUserAndAddressById(tweetId:string):Promise<[string,string]> {
+    const  [{ full_text, user: { id_str: userId } }] :any[] = await client.v1.tweets(tweetId);
+    return  [/0x[a-zA-Z0-9]{40}/.exec(full_text)![0],userId]
+} 
+
+//console.log(await name("1434123996958707717"));
+
+// Gets the useres, who retweeted the tweet specified by the param
+// Returns with an array of the reteeres' IDs
+async function getRetweetersById(tweetId:string) {
+    let retweetersId = (await client.v2.tweetRetweetedBy(tweetId)).data.map(retweet =>retweet.id);
+    return retweetersId;
+}
+
+//console.log(await getRetweetersById("1436035119610015744"));
